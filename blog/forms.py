0a478e71a791d0b15c16e529f4df47541aa97ef1
Shijin1111 <151844395+Shijin1111@users.noreply.github.com>
2024-07-21 16:25:42 +0530

    myblog completed

--- blog/forms.py
+++ blog/forms.py
@@ -7,7 +7,7 @@ class PostForm(forms.ModelForm):
         fields = ('author','title','text')
         widgets = {
             'title':forms.TextInput(attrs={'class':'textinputclass'}),
-            'text':forms.Textarea(attrs={'class':'editable medium-editor-textarea postcontent'}),            
+            'text':forms.Textarea(attrs={'class':'editable medium-editor-textarea postcontent '}),            
         }
         
 class CommentForm(forms.ModelForm):
